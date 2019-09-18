07a3bb0c6d36e792be5ef504768b018c1adf4e17
admin <646350657@qq.com>
2019-09-18 15:57:18 +0800

    commit

---
+++ src/test.java
@@ -0,0 +1,15 @@
+/**
+ * @author da xiong
+ * @date 2019/9/11 09:31
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        int i = 0;
+        i = i++;
+        System.out.println(i);
+        int j = 0;
+        j = ++j;
+        System.out.println(j);
+    }
+}
